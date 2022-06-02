a5a7c97b84b05ed61e5a1db765922311b0f42856
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-02 17:05:57 -0300

    IoC criada

---
+++ PizzaFast.Shared/IoC/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PizzaFast.Infra.Context;
+
+namespace PizzaFast.Shared.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"),
+                b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
