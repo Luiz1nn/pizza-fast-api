4f65e34b6920d63404e9d0b7e5e1bd1ef54e0612
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-02 16:55:56 -0300

    ModelsConfiguration criados

--- PizzaFast.Domain/Models/Categoria.cs
+++ PizzaFast.Domain/Models/Categoria.cs
@@ -1,8 +1,17 @@
-﻿namespace PizzaFast.Domain.Models
+﻿using System.Collections.Generic;
+
+namespace PizzaFast.Domain.Models
 {
     public class Categoria
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ICollection<Produto> Produtos { get; set; }
+
+        public Categoria(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
     }
 }
