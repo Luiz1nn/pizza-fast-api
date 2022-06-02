13ac1245f18b38c630fd8554a0bffc6dd55b098c
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-02 18:02:33 -0300

    Interfaces do Service criadas

---
+++ PizzaFast.Application/Interfaces/ICategoriaService.cs
@@ -0,0 +1,15 @@
+﻿using PizzaFast.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PizzaFast.Application.Interfaces
+{
+    public interface ICategoriaService
+    {
+        Task<IEnumerable<Categoria>> GetCategorias();
+        Task<Categoria> GetById(int id);
+        Task Add(Categoria categoria);
+        Task Update(Categoria categoria);
+        Task Remove(int id);
+    }
+}
