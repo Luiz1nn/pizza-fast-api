9391a3558cb42ed71e7c06694a464d9463b98f72
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-02 16:33:28 -0300

    Models criados

---
+++ PizzaFast.Domain/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzaFast.Domain.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Preco { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
