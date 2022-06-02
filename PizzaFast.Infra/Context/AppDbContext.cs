63886d759811e36ac44f655868ce2ab43fac0339
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-02 16:46:52 -0300

    Context criado

---
+++ PizzaFast.Infra/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaFast.Domain.Models;
+using System;
+
+namespace PizzaFast.Infra.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(AppContext).Assembly);
+        }
+    }
+}
