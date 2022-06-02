20148932a8c584bf80bc6f145645b0a93f4dcb8c
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-02 17:15:43 -0300

    Startup config

--- PizzaFast.Shared/IoC/DependencyInjection.cs
+++ PizzaFast.Shared/IoC/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PizzaFast.Infra.Context;
+using System;
 
 namespace PizzaFast.Shared.IoC
 {
@@ -9,9 +10,12 @@ namespace PizzaFast.Shared.IoC
     {
         public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
         {
+            //services.AddDbContext<AppDbContext>(options =>
+            //    options.UseSqlServer(configuration.GetConnectionString("ConnectionString"),
+            //    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
             services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"),
-                b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+                options.UseMySql(configuration.GetConnectionString("ConnectionString"), new MySqlServerVersion(new Version(8, 0, 11))));
 
             return services;
         }
