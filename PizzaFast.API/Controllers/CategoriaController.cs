7c0018be4d92fa97c90d9b23f193377e88cd481a
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-03 10:52:48 -0300

    CategoriaController criado

--- PizzaFast.API/Controllers/CategoriaController.cs
+++ PizzaFast.API/Controllers/CategoriaController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PizzaFast.Application.Interfaces;
+using PizzaFast.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PizzaFast.API.Controllers
 {
@@ -7,5 +11,43 @@ namespace PizzaFast.API.Controllers
     [ApiController]
     public class CategoriaController : ControllerBase
     {
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriaController(ICategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> Get()
+        {
+            var categorias = await _categoriaService.GetCategorias();
+            return Ok(categorias);
+        }
+
+        [HttpGet("{id}", Name = "GetCategoria")]
+        public async Task<ActionResult<Categoria>> Get(int id)
+        {
+            var categoria = _categoriaService.GetById(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _categoriaService.Add(categoria);
+
+            return new CreatedAtRouteResult("GetCategoria", new { id = categoria.Id }, categoria);
+        }
     }
 }
---
+++ PizzaFast.Application/Services/CategoriaService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using PizzaFast.Application.Interfaces;
+using PizzaFast.Domain.Interfaces;
+using PizzaFast.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PizzaFast.Application.Services
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private ICategoriaRepository _categoriaRepository;
+        private readonly IMapper _mapper;
+
+        public CategoriaService(ICategoriaRepository categoriaRepository, IMapper mapper)
+        {
+            _categoriaRepository = categoriaRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Categoria>> GetCategorias()
+        {
+            return await _categoriaRepository.GetCategoriasAsync();
+        }
+
+        public async Task<Categoria> GetById(int id)
+        {
+            return await _categoriaRepository.GetByIdAsync(id);
+        }
+
+        public async Task Add(Categoria categoria)
+        {
+            await _categoriaRepository.CreateAsync(categoria);
+        }
+
+        public async Task Update(Categoria categoria)
+        {
+            await _categoriaRepository.UpdateAsync(categoria);
+        }
+
+        public async Task Remove(int id)
+        {
+            var categoria = _categoriaRepository.GetByIdAsync(id).Result;
+            await _categoriaRepository.RemoveAsync(categoria);
+        }
+    }
+}
