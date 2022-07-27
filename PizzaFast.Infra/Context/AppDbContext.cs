b713330f7c423108e22ebb12af52d5fbf67450b0
Luis Fernando <luisfernandobr2003@gmail.com>
2022-07-27 08:22:11 -0300

    chore: Indetitu Entity FrameworkCore

--- PizzaFast.Infra/Context/AppDbContext.cs
+++ PizzaFast.Infra/Context/AppDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using PizzaFast.Domain.Models;
 using System;
 
 namespace PizzaFast.Infra.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
